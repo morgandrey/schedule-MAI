f27e759126b236ef2f95291c1a7b993a1948cd2e
Andrey <morgunof-andrey@yandex.ru>
2021-03-10 11:11:12 +0300

    Add snackbar style

--- app/src/main/java/com/example/schedulemai/utils/Utils.kt
+++ app/src/main/java/com/example/schedulemai/utils/Utils.kt
@@ -1,6 +1,8 @@
 package com.example.schedulemai.utils
 
+import android.view.Gravity
 import android.view.View
+import android.widget.TextView
 import com.example.schedulemai.R
 import com.google.android.material.snackbar.Snackbar
 
@@ -12,10 +14,16 @@ import com.google.android.material.snackbar.Snackbar
 
 object Utils {
     fun showNetworkConnectionLostSnackBar(view: View) {
-        Snackbar.make(
+        val snack = Snackbar.make(
             view,
             R.string.network_connection_lost,
             Snackbar.LENGTH_LONG
-        ).show()
+        )
+        snack.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+            .also { textView ->
+                textView.gravity = Gravity.CENTER_HORIZONTAL
+                textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }
+        snack.show()
     }
 }
